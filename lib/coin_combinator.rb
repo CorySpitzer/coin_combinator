b6a072c63c9293d244d1d61b3abcf054c0062e8e
Cory Spitzer & Kevin Chen <student@epicodus.com>
2015-04-23 11:34:22 -0700

    Passes cases with exact coin value

--- lib/coin_combinator.rb
+++ lib/coin_combinator.rb
@@ -7,38 +7,21 @@ class Fixnum
       'quarters' => 0
     }
 
-    amount = self
-    change = amount
+    coin_values = {
+      'pennies'  => 1,
+      'nickles'  => 5,
+      'dimes'    => 10,
+      'quarters' => 25
+    }
 
-    until change == 0
-      if change < 5
-        penny_quantity = change
-        coins['pennies'] = penny_quantity
-        change = change - penny_quantity
-      elsif change < 10
-        nickle_quantity = change / 5
-        coins['nickles'] = nickle_quantity
-        change = change - (nickle_quantity * 5)
-      elsif change < 25
-        dime_quantity = change / 10
-        coins['dimes'] = dime_quantity
-        change = change - (dime_quantity * 10)
-      # if amount < 5
-      #   coins['pennies'] = amount
-      # elsif amount < 10
-      #
-      #   until amount.==(0)
-      #
-      #   end
-      #   remainder = amount.%(5)
-      #   coins['pennies'] = remainder
-      #   nickle_quantity = amount / 5
-      #   coins['nickles'] = nickle_quantity
-      # elsif amount < 25
-      #
-      # end
-      end
+    coin_names = coin_values.invert
+
+    amount = self
+    if coin_values.has_value?(amount)
+      coin_name = coin_names[amount]
+      coins[coin_name] += 1
     end
+
     coins
   end
 end
